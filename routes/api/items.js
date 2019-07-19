fae94bb0ff1ec2d6aa42a0dea1c78bc81b8ab0e2
Thomas Chen <thomas.chen97@gmail.com>
2019-07-18 21:25:07 -0700

    Fix deprecated remove

--- routes/api/items.js
+++ routes/api/items.js
@@ -28,9 +28,13 @@ router.post('/', (req, res) => {
 // @desc    Delete An Item
 // @access  Public
 router.delete('/:id', (req, res) => {
-  Item.findById(req.params.id)
-    .then(item => item.remove().then(() => res.json({ success: true })))
-    .catch(err => res.status(404).json({ success: false }));
+  Item.findByIdAndRemove(req.params.id, (err) => {
+    if (err) {
+      res.status(404).json({ success: false });
+    } else {
+      res.status(200).json({ success: true });
+    }
+  })
 });
 
 module.exports = router;
